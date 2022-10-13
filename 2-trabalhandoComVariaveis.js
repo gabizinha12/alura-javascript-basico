8149728196dc516e2444eec9f65a2435324362ab
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-10-13 09:24:59 -0300

    Curso de javascript

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com variáveis");
+// js case sensitive
+
+//idade = 29;
+//Idade = 30;
+
+//console.log(idade);
+//console.log(Idade);
+
+const idade = 21;
+const nome = "Gabi";
+const ano = 2022;
